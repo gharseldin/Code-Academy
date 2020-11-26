7d6dfcd4410f82f1c1066e79aac05920d853b99e
gharseldin <gharseldin@aucegypt.edu>
2020-11-26 15:35:21 -0800

    Initial Commit

---
+++ Code Academy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Code Academy
+//
+//  Created by Amr Gharseldin on 11/26/20.
+//  Copyright © 2020 Amr Gharseldin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
